d8a44afcc404e3b2dcc5f05e5e2b24559449f9b0
Krish Pathak <krishpathak20@gmail.com>
2024-08-01 21:50:52 +0530

    first

--- backend/db.js
+++ backend/db.js
@@ -2,10 +2,10 @@ require('dotenv').config();
 const mysql = require('mysql2');
 
 const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    host: 'localhost',
+    user: 'root',
+    password: 'KrishPathak25',
+    database: 'krishdb'
 });
 
 db.connect((err) => {

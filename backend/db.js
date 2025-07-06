aef8b41bb05f7747ed1549e2cbe65ad8fd974fb6
Krish Pathak <142569867+krishpathak@users.noreply.github.com>
2025-07-06 11:11:39 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,12 +1,15 @@
 require('dotenv').config();
 const mysql = require('mysql2');
+require('dotenv').config();
+const mysql = require('mysql');
 
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'KrishPathak25',
-    database: 'krishdb'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 });
+
 
 db.connect((err) => {
     if (err) {

83b3deb8d6e4f856560028c2dd645399060f4946
Krish Pathak <142569867+krishpathak@users.noreply.github.com>
2025-07-06 11:04:33 +0530

    Update index.js

--- backend/cloudinary/index.js
+++ backend/cloudinary/index.js
@@ -1,11 +1,14 @@
 const cloudinary = require('cloudinary').v2;
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
+require('dotenv').config();
+const cloudinary = require('cloudinary').v2;
 
 cloudinary.config({
-    cloud_name:"dvpvxjcfk",
-    api_key:286473956267266,
-    api_secret:"zQa7HNHDlGdyPmCsRcnG2sJpVt4"
-})
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
 
 const storage=new CloudinaryStorage({
     cloudinary,
